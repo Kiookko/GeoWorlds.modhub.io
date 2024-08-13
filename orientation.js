9e2c41db102a992cbea4794022cfcc6fcaf90571
Kiookko <166719494+Kiookko@users.noreply.github.com>
2024-08-13 09:59:04 +0500

    Update orientation.js

--- orientation.js
+++ orientation.js
@@ -1,19 +1,9 @@
-function adjustLayout() {
-    const image = document.getElementById("responsiveImage");
-
-    if (window.innerHeight > window.innerWidth) {
-        console.log('Portrait mode');
-        image.style.width = "auto";
-        image.style.height = "100vh";
-    } else {
-        console.log('Landscape mode');
-        image.style.width = "100vw";
-        image.style.height = "auto";
-    }
-}
-
-window.addEventListener('orientationchange', adjustLayout);
-window.addEventListener('resize', adjustLayout);
-
-// Initial call to set the correct layout
-document.addEventListener("DOMContentLoaded", adjustLayout);
+function adjustLayout() { const image = document.getElementById("responsiveImage");if (window.innerHeight > window.innerWidth) {
+    console.log('Portrait mode');
+    image.style.width = "auto";
+    image.style.height = "100vh";
+} else {
+    console.log('Landscape mode');
+    image.style.width = "100vw";
+    image.style.height = "auto";
+}}window.addEventListener('orientationchange', adjustLayout); window.addEventListener('resize', adjustLayout);// Initial call to set the correct layout document.addEventListener("DOMContentLoaded", adjustLayout);
